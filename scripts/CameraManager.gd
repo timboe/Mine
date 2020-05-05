b4567c464dc3586de74e5c3ba6488f6d6e109341
Tim Martin <timboe@gmail.com>
2020-05-05 22:34:02 +0100

    Add possession

--- scripts/CameraManager.gd
+++ scripts/CameraManager.gd
@@ -1,23 +1,28 @@
 extends Node
 
 onready var player : KinematicBody = $"../Player"
+onready var rot_helper : Camera = $"../Player/Rotation_Helper"
 onready var fps_camera : Camera = $"../Player/Rotation_Helper/Camera"
-onready var rot_helper : Spatial = $"../Player/Rotation_Helper"
 onready var overhead_camera : Camera = $"../Camera"
 onready var overhead_light : OmniLight = $"../OmniLight"
 onready var tween : Tween = $Tween
+onready var spawn_particles : Particles = $SpawnParticles
 
-var overhead_quat : Quat
-var fps_quat : Quat
+var quat_from : Quat
+var quat_to : Quat
 
 const TRANSITION_TIME : float = 2.0
+const PLAYER_LOWER_DEPTH : float = 5.0
+const UNPOSESS_DISTANCE := Vector2(-40, 50)
+
+const TRANS := Tween.TRANS_SINE
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-func quat_transform_overhead_to_fps(var amount : float):
-	var mid = overhead_quat.slerp(fps_quat, amount)
+func quat_transform(var amount : float):
+	var mid = quat_from.slerp(quat_to, amount)
 	overhead_camera.transform.basis = Basis(mid)
 
 func _input(event):
@@ -35,52 +40,73 @@ func to_fps_cam_start():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if GlobalVars.SELECTED_NODE != null:
 		GlobalVars.SELECTED_NODE.call("_on_StaticBody_mouse_exited")
-	player.translation = overhead_light.to_global(Vector3.ZERO)
-	player.translation.y = 0 if overhead_light.floor_lowered else GlobalVars.FLOOR_HEIGHT
+	player.transform.origin = overhead_light.to_global(Vector3.ZERO)
+	player.transform.origin.y = 0 if overhead_light.floor_lowered else GlobalVars.FLOOR_HEIGHT
 	player.look_at( overhead_camera.to_global(Vector3.ZERO), Vector3.UP)
 	player.rotate_object_local(Vector3.UP, PI) # Actually, look AWAY
+	# Keep the actual rotation around x on the camera
+	#TODO clamp isn't working
+	fps_camera.rotation.x = player.rotation.x
+	fps_camera.rotation.x = clamp(fps_camera.rotation.x, -70, 70)
+	rot_helper.rotation.x = fps_camera.rotation.x
+	player.rotation.x = 0
 	var player_target = player.to_global(Vector3.ZERO)
 	var camera_target = fps_camera.to_global(Vector3.ZERO)
-	overhead_quat = Quat(overhead_camera.transform.basis)
-	fps_quat = Quat(player.transform.basis)
-	player.transform.origin.y -= 3.0 # Hide underneath
-	# Keep the actual rotation around x on the helper only
-	rot_helper.rotation.x = player.rotation.x
-	player.rotation.x = 0
-	#print("GOING TO " , fps_camera.to_global(Vector3.ZERO))
+	quat_from = Quat(overhead_camera.transform.basis)
+	quat_to = Quat(fps_camera.get_global_transform().basis)
+	# Move the spawn effect here too now
+	spawn_particles.transform.origin = player.transform.origin
+	spawn_particles.emitting = true
+	player.transform.origin.y -= PLAYER_LOWER_DEPTH # Hide underneath
+
 	tween.interpolate_property(player, "translation",
-		null, player_target, TRANSITION_TIME, Tween.TRANS_CIRC, Tween.EASE_OUT)
+		null, player_target, TRANSITION_TIME, TRANS, Tween.EASE_IN_OUT)
 	tween.interpolate_property(overhead_camera, "translation",
-		overhead_camera.to_global(Vector3.ZERO), camera_target, 2.0, Tween.TRANS_CIRC, Tween.EASE_IN)
-	tween.interpolate_method(self, "quat_transform_overhead_to_fps",
-		0.0, 1.0, TRANSITION_TIME, Tween.TRANS_CIRC, Tween.EASE_IN)
+		overhead_camera.to_global(Vector3.ZERO), camera_target,
+		TRANSITION_TIME, TRANS, Tween.EASE_OUT)
+	tween.interpolate_method(self, "quat_transform",
+		0.0, 1.0, TRANSITION_TIME, TRANS, Tween.EASE_OUT)
 	tween.interpolate_callback(self, TRANSITION_TIME, "to_fps_cam_end")
 	tween.start()
+
 
 func to_fps_cam_end():
 	GlobalVars.camera_status = GlobalVars.CameraStatus.FPS
 	overhead_camera.current = false
 	fps_camera.current = true
-	#print("B Gcam height loc ", root_camera.translation, " glob ",root_camera.to_global(root_camera.translation))
-	#print("B  cam height loc ", camera.translation, " glob ",camera.to_global(camera.translation))
-	print("AT " , overhead_camera.to_global(Vector3.ZERO))
+	spawn_particles.emitting = false
 
 func to_overhead_cam_start():
 	GlobalVars.camera_status = GlobalVars.CameraStatus.TO_OVERHEAD
-	overhead_camera.translation = fps_camera.to_global(fps_camera.translation)
-	overhead_camera.rotation = player.rotation
-	var target_pos = overhead_camera.translation
-	target_pos.y += 20
-	#var target_rot = 
+	var start : Vector3 = fps_camera.to_global(Vector3.ZERO)
+	var cam_xform = fps_camera.get_global_transform()
+	# Go first to the final position
+	overhead_camera.transform = cam_xform
+	# Move back by 20m and set to 50m height
+	overhead_camera.transform.origin += -cam_xform.basis.z * UNPOSESS_DISTANCE.x
+	overhead_camera.transform.origin.y = UNPOSESS_DISTANCE.y
+	overhead_camera.look_at(start, Vector3.UP)
+	var target_tf : Transform = overhead_camera.transform
+	var player_target : Vector3 = player.to_global(Vector3.ZERO)
+	player_target.y -= PLAYER_LOWER_DEPTH
+	# Set to same as the fps_cam
+	overhead_camera.transform = cam_xform
 	overhead_camera.current = true
 	fps_camera.current = false
+	quat_from = Quat(cam_xform.basis)
+	quat_to = Quat(target_tf.basis)
 	tween.interpolate_property(overhead_camera, "translation",
-		null, target_pos, 2.0, Tween.TRANS_CIRC, Tween.EASE_IN)
-	#tween.interpolate_property(overhead_camera, "rotation",
-	#	rotation, overhead_camera.rotation, 2.0, Tween.TRANS_CIRC, Tween.EASE_IN)
-	tween.interpolate_callback(self, 3.0, "to_overhead_cam_end")
+		null, target_tf.origin, TRANSITION_TIME, TRANS, Tween.EASE_OUT)
+	tween.interpolate_property(player, "translation",
+		null, player_target, TRANSITION_TIME, TRANS, Tween.EASE_OUT)
+	tween.interpolate_method(self, "quat_transform",
+		0.0, 1.0, TRANSITION_TIME, TRANS, Tween.EASE_OUT)
+	tween.interpolate_callback(self, TRANSITION_TIME, "to_overhead_cam_end")
 	tween.start()
+	spawn_particles.transform.origin = player.transform.origin
+	spawn_particles.emitting = true
 
 func to_overhead_cam_end():
 	GlobalVars.camera_status = GlobalVars.CameraStatus.OVERHEAD
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	spawn_particles.emitting = false
